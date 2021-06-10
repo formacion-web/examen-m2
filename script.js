95d71f4b96d46143a103a3dd4d164cd97b885768
olejandr <sora.oleksandr@gmail.com>
2021-06-10 12:39:59 +0200

    examen

--- script.js
+++ script.js
@@ -3,46 +3,88 @@
  * Utilízalos si quieres.
  */
 
-class Starwars {
-    
-
-
-  getFilms() {
-    let urlFilms = "https://www.swapi.tech/api/films";
-    // const fetchFilms = async (urlFilms) => {
-    //   try {
-    //     let response = await fetch(urlFilms);
-    //     if (response.ok) {
-    //       let data = await response.json();
-    //       return data;
-    //     }
-    //   } catch (error) {
-    //     throw error;
-    //   }
-    // };
-    fetch(urlFilms).then(result => result.json())
-                   .then(data => {return data})
-                   .catch(error => {throw error})
-    
+let urlFilms = "https://www.swapi.tech/api/films";
+const fetchFilms = async (urlFilms) => {
+  try {
+    let response = await fetch(urlFilms);
+    if (response.ok) {
+      let data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    throw error;
   }
+};
+// fetch(urlFilms).then(result => result.json())
+//                .then(data => {return data})
+//                .catch(error => {throw error})
 
-  getFilmById(id) {
-    let datos = getFilms(urlFilms);
-    datos.forEach(e=> {
-        let title = e.title;
-        let opening_crawl = e.opening_crawl
-    })
-  }
+// const getFilms = async () => {
+//   const datos = await fetchFilms(urlFilms);
+//   let lista = document.getElementById("lista");
+//   datos.forEach((e) => {
+//     let title = e.title;
 
-  // getCharacters() {
-  // }
-  // getCharactersById(id) {
-  // }
-}
+//     const li = document.createElement("li");
 
-const starwars = new Starwars();
+//     li.appendChild(title);
+//     lista.appendChild(li);
+//   });
+// };
 
-const loadFilmTitles = () => {};
+// class Starwars {
+
+//   getFilms() {
+//     let urlFilms = "https://www.swapi.tech/api/films";
+//     // const fetchFilms = async (urlFilms) => {
+//     //   try {
+//     //     let response = await fetch(urlFilms);
+//     //     if (response.ok) {
+//     //       let data = await response.json();
+//     //       return data;
+//     //     }
+//     //   } catch (error) {
+//     //     throw error;
+//     //   }
+//     // };
+//     fetch(urlFilms).then(result => result.json())
+//                    .then(data => {return data})
+//                    .catch(error => {throw error})
+
+//   }
+
+//   getFilmById(id) {
+//     let datos = getFilms(urlFilms);
+//     datos.forEach(e=> {
+//         let title = e.title;
+//         let opening_crawl = e.opening_crawl;
+
+//     })
+//   }
+
+//   // getCharacters() {
+//   // }
+//   // getCharactersById(id) {
+//   // }
+// }
+
+// const starwars = new Starwars();
+
+const loadFilmTitles = async () => {
+  const datos = await fetchFilms(urlFilms);
+  let lista = document.querySelector("lista");
+
+  datos.forEach((e) => {
+    let title = e.title;
+
+    const li = document.createElement("li");
+
+    li.textContent = title;
+
+   
+    lista.appendChild(li);
+  })
+};
 
 const init = () => {
   document
