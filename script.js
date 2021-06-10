478e74c5529fd4fa9a6949459292a5d3a399899a
olejandr <sora.oleksandr@gmail.com>
2021-06-10 11:50:44 +0200

    examen

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 /**
- * Todos los nombres de métodos y funciones son orientativos. 
+ * Todos los nombres de métodos y funciones son orientativos.
  * Utilízalos si quieres.
  */
 
 class Starwars {
-    getFilms() {
+    
 
-    }
 
-    getFilmById(id) {
-    }
-    getCharacters() {
-    }
-    getCharactersById(id) {
-    }
+  getFilms() {
+    let urlFilms = "https://www.swapi.tech/api/films";
+    // const fetchFilms = async (urlFilms) => {
+    //   try {
+    //     let response = await fetch(urlFilms);
+    //     if (response.ok) {
+    //       let data = await response.json();
+    //       return data;
+    //     }
+    //   } catch (error) {
+    //     throw error;
+    //   }
+    // };
+    fetch(urlFilms).then(result => result.json())
+                   .then(data => {return data})
+                   .catch(error => {throw error})
+    
+  }
 
+  getFilmById(id) {
+    let datos = getFilms(urlFilms);
+    datos.forEach(e=> {
+        let title = e.title;
+        let opening_crawl = e.opening_crawl
+    })
+  }
+
+  // getCharacters() {
+  // }
+  // getCharactersById(id) {
+  // }
 }
 
 const starwars = new Starwars();
 
-const loadFilmTitles = ()=>{
-
-}
+const loadFilmTitles = () => {};
 
 const init = () => {
-
-    document.getElementById('peliculas').addEventListener('click', loadFilmTitles);
-
-}
+  document
+    .getElementById("peliculas")
+    .addEventListener("click", loadFilmTitles);
+};
 
 onload = init;
