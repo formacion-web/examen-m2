c586933c0b8897aa78b38d16014d598a4e97390d
r4kogama <thecureomd@gmail.com>
2021-06-10 13:23:42 +0200

    examen

--- script.js
+++ script.js
@@ -3,12 +3,22 @@
  * Utilízalos si quieres.
  */
 
+let peliculas =  [];
 class Starwars {
-    getFilms() {
-
+    
+    getFilms(films) {
+        films.result.forEach((e) => {
+            peliculas.push(e);
+        });
+        console.log(peliculas);
+        return peliculas;
     }
 
     getFilmById(id) {
+        peliculas.forEach((e) => {
+            return  e[id];
+        });
+
     }
     getCharacters() {
     }
@@ -19,8 +29,34 @@ class Starwars {
 
 const starwars = new Starwars();
 
-const loadFilmTitles = ()=>{
 
+const urlFetch = async() =>{
+    let ul = document.querySelector('.lista');
+    try{
+        let response = await (fetch('./starwars.json'));
+        if(response.ok){
+            let responseData = await response.json();
+            let movies = starwars.getFilms(responseData);
+            ul.innerHTML= '';
+             movies.forEach(res => {
+                 ul.innerHTML += `<li><a onclick="moviesById(this)" id='${res.uid}'>${res.properties.title}</a></li>`;
+            });
+        }else{
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    }catch(err){
+        throw  new Error(`HTTP error! status: ${err}`);
+    }
+}
+
+const moviesById = (e)=>{
+    let content = starwars.getFilmById(e.getAttribute("id"));
+    console.log(content);
+}
+
+
+const loadFilmTitles =  ()=>{
+    urlFetch();
 }
 
 const init = () => {
